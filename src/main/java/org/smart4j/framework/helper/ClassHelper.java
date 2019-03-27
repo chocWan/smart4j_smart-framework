89038cbdbff3b00ef013664f29d7f6f24c2bd823
chocWan <13545689480@163.com>
2019-03-27 09:34:45 +0800

    d

---
+++ src/main/java/org/smart4j/framework/helper/ClassHelper.java
@@ -0,0 +1,42 @@
+package org.smart4j.framework.helper;
+
+import org.smart4j.framework.annotation.Service;
+import org.smart4j.framework.util.ClassUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class ClassHelper {
+
+    private static final Set<Class<?>> CLASS_SET;
+
+    static {
+        String basePackage = ConfigHelper.getAppBasePackage();
+        CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    private Set<Class<?>> getClassSet() {
+        return CLASS_SET;
+    }
+
+    public Set<Class<?>> getSerivceClassSet(){
+        Set<Class<?>> services = new HashSet<Class<?>>();
+        for(Class<?> cls : CLASS_SET) {
+            if(cls.isAnnotationPresent(Service.class)){
+                services.add(cls);
+            }
+        }
+        return services;
+    }
+
+    public Set<Class<?>> getControllerClassSet(){
+        Set<Class<?>> services = new HashSet<Class<?>>();
+        for(Class<?> cls : CLASS_SET) {
+            if(cls.isAnnotationPresent(Service.class)){
+                services.add(cls);
+            }
+        }
+        return services;
+    }
+
+}
